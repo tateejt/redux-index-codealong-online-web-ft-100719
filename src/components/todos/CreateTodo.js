1fb947472dad8cf7b03fb8b84dd07e230406c3ff
Emery Tate <tateejt@gmail.com>
2020-03-15 01:58:43 +0000

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -21,6 +21,14 @@ class CreateTodo extends Component {
     });
   }
 
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addTodo(this.state)
+    this.setState({
+      text: '',
+    })
+  }
+  
   render() {
     return(
       <div>
